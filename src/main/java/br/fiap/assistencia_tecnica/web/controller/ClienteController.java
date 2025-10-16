ec63785efdb742da8663579e8d2fd52e689433a6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-16 11:43:56 -0300

    first commit

---
+++ src/main/java/br/fiap/assistencia_tecnica/web/controller/ClienteController.java
@@ -0,0 +1,23 @@
+package br.fiap.assistencia_tecnica.web.controller;
+
+import br.fiap.assistencia_tecnica.service.ClienteService;
+import br.fiap.assistencia_tecnica.web.dto.ClienteDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    private final ClienteService service;
+
+    public ClienteController(ClienteService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public void cadastrar(@RequestBody ClienteDTO clienteDTO){
+
+    }
+
+}
---
+++ src/main/java/br/fiap/assistencia_tecnica/web/dto/ClienteDTO.java
@@ -0,0 +1,50 @@
+package br.fiap.assistencia_tecnica.web.dto;
+
+public class ClienteDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
